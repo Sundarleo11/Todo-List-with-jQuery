41b4edf155e6944bdc499fb7d49ae697e6ae50ac
Sundarleo11 <sundarrock1999@gmail.comm>
2021-12-18 23:11:23 +0530

    addedbutton

--- script.js
+++ script.js
@@ -3,17 +3,48 @@ var add = $("#add");
 var clear = $("#clear");
 var task = $("#task");
 
+function additerm(){
+  var item = $("<li>", {
+    class: "list-group-item",
+    text: task.val(),
+  })
+
+  item.click((event) => {
+   item.toggleClass("done");
+  })
+  ullist.append(item);
+  task.val("");
+
+}
+
+task.keypress((e)=>{
+//console.log(e.which);
+if(e.which==13) additerm();
+}),
+
+
+add.click(additerm);
+/*
 add.click(() => {
   // alert('hi');
   // console.log(task.val());
-  let item = $("<li>", {
+  var item = $("<li>", {
     class: "list-group-item",
     text: task.val(),
-  });
+  })
+
+  item.click((event) => {
+    //console.log("click",$(this));
+   // console.log(event, $(this));
+   //console.log("click", $(event.target));
+   //  $(event.target).toggleClass("disabled");
+   // item.toggleClass("disabled");
+     item.toggleClass("done");
+  })
   ullist.append(item);
   task.val("");
 });
-
+*/
 clear.click(() => {
   task.val("");
 });
